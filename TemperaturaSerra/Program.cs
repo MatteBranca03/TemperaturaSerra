0d33d06cf1b6d3b5704626fd1835af1515edc014
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:43:59 +0200

    cambiamenti

--- TemperaturaSerra/Program.cs
+++ TemperaturaSerra/Program.cs
@@ -6,7 +6,23 @@ namespace TemperaturaSerra
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("inserisci la temperatura della serra");
+            double t = double.Parse(Console.ReadLine());
+            if (t < 5)
+            {
+                Console.WriteLine($"la temperatura è {t}, ATTENZIONE danni irreparabili");
+                Console.ReadLine();
+            }
+            if (t > 5 && t < 18)
+            {
+                Console.WriteLine($"la temperatura è {t}, ATTENZIONE situazione di pericolo");
+                Console.ReadLine();
+            }
+            if (t == 18)
+            {
+                Console.WriteLine($"la temperatura è {t}, la serra è in una temperatura normale");
+                Console.ReadLine();
+            }
         }
     }
 }
