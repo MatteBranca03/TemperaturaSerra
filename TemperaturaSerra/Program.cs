31aa37bc325cf748c3ab4938ab2b3baa367bce1e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 11:55:55 +0200

    Add project files.

---
+++ TemperaturaSerra/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemperaturaSerra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
